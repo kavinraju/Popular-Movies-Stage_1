d4c497c9db3f4ec0e9abdbe73e5d11cbf43e13b1
KavinRaju <rajukavin25@gmail.com>
2018-06-14 17:01:43 +0530

    Added comments

--- app/src/main/java/com/example/kavinraju/popularmovies/HelperClass/BottomNavigationBehavior.java
+++ app/src/main/java/com/example/kavinraju/popularmovies/HelperClass/BottomNavigationBehavior.java
@@ -9,6 +9,9 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+/*
+    This class supports Animation of the BottomNavigationView when the user scrolls down.
+ */
 public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomNavigationView> {
 
     private int height;
@@ -30,6 +33,7 @@ public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomN
     @Override
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull BottomNavigationView child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
         Log.d(className , "onStartNestedScroll");
+        // return true if the scroll axis is vertical
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 
@@ -39,10 +43,18 @@ public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomN
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
 
         if ( dyConsumed > 0 ){
+            /*
+            if the user scrolls down then the BottomNavigationView is translated down by its height, so that it disappears from the screen
+            and provides the user with more space to explore on the screen.
+             */
             child.clearAnimation();
             child.animate().translationY(height).setDuration(200);
             Log.d(className , "onNestedPreScroll: dy < 0" + " Type" + String.valueOf(type));
         }else if ( dyConsumed < 0 ){
+            /*
+            if the user scrolls up then the BottomNavigationView is translated up to its original position,
+            so that it appears again on the screen
+             */
             child.clearAnimation();
             child.animate().translationY(0).setDuration(200);
             Log.d(className , "onNestedPreScroll: dy > 0" + " Type" + String.valueOf(type));
